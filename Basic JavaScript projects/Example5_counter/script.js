602e068aeeab952b9d2ba4da25378a4c5c665437
Marko Mikec <mikec.marko@protonmail.com>
2020-10-24 15:13:50 +0200

    Example5_counter

--- Basic JavaScript projects/Example5_counter/script.js
+++ Basic JavaScript projects/Example5_counter/script.js
@@ -1,22 +1,38 @@
-const sendbutton = document.querySelector('#sendbutton');
-const messageIn = document.querySelector('#messageIn');
-const messageOut = document.querySelector('#messageOut');
+let countner = document.querySelector('.counter');
+const increase = document.querySelector('#IncreaseCountButton');
+const lower = document.querySelector('#LowerCountButton');
+
+let count = 0;
+
+increase.addEventListener('click',increaser);
+lower.addEventListener('click',lowerer);
 
 
-sendbutton.addEventListener('click',sendmessage)
-
-function sendmessage(){
-    //collecting a value from input
-    let content = messageIn.value;
-
-    if(content===''){
-        alert('Please enter the required information!')
+// we can look for elemeent syntax on certain web browsers
+function increaser(){
+    count++;
+    countner.innerHTML=count;
+    if(countner.innerHTML>'0'){
+        countner.style.color = 'green';
     }
-    else{
-        // to check console.log(content);
-        messageOut.innerHTML = content; 
-        messageIn.value='';
-
-    }    
-
+    else if(countner.innerHTML==='0'){
+        countner.style.color = 'white';
+    }
+    countner.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:2000,fill:'forwards'})
 }
+function lowerer(){
+    count--;
+    countner.innerHTML=count;
+    
+    if(countner.innerHTML<'0'){
+        countner.style.color = 'red';
+    }
+    else if(countner.innerHTML==='0'){
+        countner.style.color = 'white';
+    }
+    countner.animate([{opacity:'0.2'},{opacity:'1.0'}],{duration:2000,fill:'forwards'})
+}
+
+//color change for animate effect 
+
+
