25e744179887d17b01e218024c40da84256dc756
Marko Mikec <mikec.marko@protonmail.com>
2021-02-14 00:12:43 +0100

    Website update

--- WebPage project/app.js
+++ WebPage project/app.js
@@ -1,2 +1,11 @@
 const menu = document.querySelector('#mobile-menu');
-const menuLink = document.querySelector('.navbar__menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+//Display Mobile Menu
+
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
