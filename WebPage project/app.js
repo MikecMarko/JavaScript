b613229e89c1a01bfbae28aab6e507bfd8e50bda
Marko Mikec <mikec.marko@protonmail.com>
2021-02-20 20:29:15 +0100

    Webpage work

--- WebPage project/app.js
+++ WebPage project/app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 //Display Mobile Menu
 
@@ -9,3 +10,12 @@ const mobileMenu = () => {
 };
 
 menu.addEventListener('click', mobileMenu);
+
+//Show active menu when scrolling
+
+const highlightMenu = () => {
+  const element = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('.#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const servicesMenu = document.querySelector('#services-page');
+};
