70bffca5f07041b89fbebb9ac36004a16a03cd0b
Marko Mikec <mikec.marko@protonmail.com>
2021-02-23 23:55:36 +0100

    New JS project

--- JS New Projects/home.js
+++ JS New Projects/home.js
@@ -1,2 +1,16 @@
 console.log('Hi');
+
+//Inline comment
 //alert('yoo');
+
+{
+  /*Variables*/
+}
+
+var b = 'This is variable';
+console.log(b);
+
+var number = 45;
+console.log(number);
+
+document.getElementById('paragraph').textContent = 'This is a paragraph';
