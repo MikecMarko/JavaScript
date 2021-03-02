90b6a3dfce85ace4d3a2bce618cbbe7906dab1f6
Marko Mikec <mikec.marko@protonmail.com>
2021-03-02 19:17:01 +0100

    JS smaller project

--- JS New Projects/home.js
+++ JS New Projects/home.js
@@ -7,6 +7,9 @@ console.log('Hi');
   /*Variables*/
 }
 
+{
+  /*
+  CHAPTER 1
 var b = 'This is variable';
 console.log(b);
 
@@ -14,3 +17,10 @@ var number = 45;
 console.log(number);
 
 document.getElementById('paragraph').textContent = 'This is a paragraph';
+
+var age = prompt('What is your age?');
+
+document.getElementById('paragraph').textContent = age;
+
+*/
+}
