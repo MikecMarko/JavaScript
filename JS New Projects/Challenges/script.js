29fd340919b186542e3dabd65801a9800167d77e
Marko Mikec <mikec.marko@protonmail.com>
2021-03-06 22:25:37 +0100

    JS project

--- JS New Projects/Challenges/script.js
+++ JS New Projects/Challenges/script.js
@@ -1,1 +1,16 @@
-//alert('hello');
+//Part 1 script
+function ageInDays() {
+  let birthYear = prompt('Please write your age');
+  let ageDays = (2021 - birthYear) * 365;
+  //Creating an H3 element
+  let h3 = document.createElement('h3');
+
+  let result = document.createTextNode('You are ' + ageDays + ' days old');
+  h3.setAttribute('id', 'calculator');
+  h3.appendChild(result);
+  document.getElementById('result').appendChild(h3);
+}
+
+function reset() {
+  document.getElementById('calculator').remove();
+}
